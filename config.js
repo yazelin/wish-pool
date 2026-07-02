5cbcc5feb0286932638f6c8964dc4fe835aad5ef
yazelin <yazelin@ching-tech.com>
2026-07-02 12:17:19 +0800

    chore: switch to real Turnstile site key (Invisible widget, yazelin.github.io)

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // TURNSTILE_SITE_KEY 改成你的 Turnstile site key(public)。
 window.WISHPOOL_CONFIG = {
   WORKER_BASE: 'https://wish-pool.yazelinj303.workers.dev',
-  TURNSTILE_SITE_KEY: '1x00000000000000000000AA', // Cloudflare 測試用 site key(永遠通過);正式上線改成真的
+  TURNSTILE_SITE_KEY: '0x4AAAAAADuSV2wW2GraJPZR',
 }
